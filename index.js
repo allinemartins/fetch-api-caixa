f629d5f4278d281fd5e4c743f2099429a30d8bee
Alline Martins <allinemartins@msn.com>
2024-03-19 17:48:18 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000; // Porta na qual o servidor irá escutar
+
+app.get('/:variable/:code', async (req, res) => {
+    const { variable, code } = req.params;
+
+    try {
+        const url = `https://servicebus2.caixa.gov.br/portaldeloterias/api/${variable}/${code}`;
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+        res.status(500).json({ error: 'Erro ao buscar dados da API' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
